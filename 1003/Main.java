66dac7c3b741a327b86c1a4f0c0a7a85c7ea008d
cfly <cfly.hi@gmail.com>
2014-05-15 11:47:41 -0400

    1003

--- 1003/Main.java
+++ 1003/Main.java
@@ -1,19 +1,32 @@
-package org.caofei.poj.p1003;
-
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String args[]) throws Exception {
-		double[] v = new double[300];
-		v[0] = -1;
-		System.out.println(calcV(v, 30));
-	}
-
-	private static double calcV(double[] v, int i) {
-		if (v[i] != -1) {
-			return v[i] = (double)1/(i+1) + calcV(v, i - 1);
-		} else {
-			return 0;
-		}
-	}
-
+public static void main(String args[]){
+double[] v = new double[276];
+calcV(v, 275);
+double f = 5.19;
+Scanner scanner = new Scanner(System.in);
+String a= scanner.next();
+while (!"0.00".equals(a)) {
+f=Double.valueOf(a);
+System.out.println(locate(v, f) + " card(s)");
+a = scanner.next();
 }
+}
+static double calcV(double[] v, int i) {
+return i != -1 ? v[i] = (double) 1 / (i + 2) + calcV(v, i - 1) : 0;
+}
+static int locate(double[] v, double f) {
+if (f <= 0.5) {
+return 1;
+}
+int s = v.length >> 1, l = s;
+while (!(f > v[l] && f <= v[l + 1])) {
+s >>= 1;
+if (f <= v[l]) {
+l -= s + 1;
+} else {
+l += s + 1;
+}}
+return l + 2;
+}}
